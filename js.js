cb1706e54c8672c70da699f5abb5d2a52f543865
sharevip <qsyshare@163.com>
2025-09-07 17:30:57 +0800

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ function showFriendlyAlert(event) {
     event.preventDefault(); // 阻止默认链接跳转
     Swal.fire({
         title: '温馨提示',
-        html: '请登录后发送您的问题，耐心等待客服，5分钟内会有回复。<br>或者 记住聊天的账号和密码，12小时内查看回复。',
+        html: '请登录后发送您的问题，<br/>耐心等待客服，5分钟内会有回复。<br/>或者 记住聊天的账号和密码，12小时内查看回复。',
         icon: 'info',
         confirmButtonText: '好的',
         customClass: {
